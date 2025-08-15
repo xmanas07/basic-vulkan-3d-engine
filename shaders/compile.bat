9771837c7ba2eae0a99d4a85a13249fdeeaeca37
xmanas07 <tomas.manasek15@seznam.cz>
2025-08-15 23:41:32 +0200

    Automated shader compilation

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,16 @@
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
-pause
+cd /d "%~dp0"
+echo Compiling shaders...
+
+rem Loop through all .vert files
+for %%f in (*.vert) do (
+    echo  - %%f
+    C:\VulkanSDK\1.4.321.1\Bin\glslc.exe "%%f" -o "%%f.spv"
+)
+
+rem Loop through all .frag files
+for %%f in (*.frag) do (
+    echo  - %%f
+    C:\VulkanSDK\1.4.321.1\Bin\glslc.exe "%%f" -o "%%f.spv"
+)
+
+echo Done!
