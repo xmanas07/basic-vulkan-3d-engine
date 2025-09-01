053480c1fcd6b65749267681eba9ef6445cf00aa
xmanas07 <tomas.manasek15@seznam.cz>
2025-09-01 17:28:02 +0200

    Fixed an issue with creating too many semaphores

--- lve_swap_chain.cpp
+++ lve_swap_chain.cpp
@@ -364,8 +364,6 @@ namespace lve {
         for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
             if (vkCreateSemaphore(device.device(), &semaphoreInfo, nullptr, &imageAvailableSemaphores[i]) !=
                 VK_SUCCESS ||
-                vkCreateSemaphore(device.device(), &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) !=
-                VK_SUCCESS ||
                 vkCreateFence(device.device(), &fenceInfo, nullptr, &inFlightFences[i]) != VK_SUCCESS) {
                 throw std::runtime_error("failed to create synchronization objects for a frame!");
             }
